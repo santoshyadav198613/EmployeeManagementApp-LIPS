472ad9bd5ec575f8136bf4ff9944c87088f687bc
Sonali <sonalipawde23@gmail.com>
2018-01-14 11:32:04 +0530

    added activatedroute service

--- src/app/comments/comments-details/comments-details.component.ts
+++ src/app/comments/comments-details/comments-details.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-comments-details',
   templateUrl: './comments-details.component.html',
   styleUrls: ['./comments-details.component.css']
 })
 export class CommentsDetailsComponent implements OnInit {
-
-  constructor() { }
+  id: number;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(
+      (data) => this.id = + data.get('id')
+    )
   }
 
 }
